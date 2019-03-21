fc0ed78ca04c832519a85f3002b17e6e83b621c1
Patrick <dawnerz@163.com>
2019-03-21 11:41:17 +0800

    nonparameter estimation test

--- K-nearest/NonParameterEstimate.py
+++ K-nearest/NonParameterEstimate.py
@@ -2,15 +2,40 @@ import numpy as np
 import math
 def knearestEstimate(sampleset,k):
     n,d=np.shape(sampleset)
-    m=(k/n**0.5)%1
+    m=(k*n**0.5)
     def probability(sample):
         dist=[]
         for p in sampleset-sample:
             dist.append((np.sum(p**2))**0.5)
-        dist=np.sort(np.array(dist))
-        r=dist[m-1]
+        np.sort(np.array(dist))
+        r=dist[int(m)]
         v=math.pi**(d/2)*r**d/math.gamma(d/2+1)
         return m/n/v
+    return probability
 if __name__=='__main__':
-    pass
-
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib.ticker import LinearLocator, FormatStrFormatter
+    def samplePoint(mean,cov,x0,x1):
+        x=np.mat([x0,x1])
+        mean=np.mat(mean)
+        cov=np.mat(cov)
+        p=len(mean)
+        y=(2*math.pi)**(p/(-2))
+        y=y*(np.linalg.det(cov))**(-0.5)
+        t=(x-mean)*np.linalg.inv(cov)*(x-mean).T
+        y=y*np.e**(-0.5*t[0,0])
+        return [x0,x1,y]
+    sampleset=np.random.multivariate_normal(mean=[0,0],cov=[[1,0],[0,1]],size=1024)
+    densityfunc=knearestEstimate(sampleset,4)
+    estipoints=[]
+    samppoints=[]
+    for i in range(0,64):
+        estipoints.append([sampleset[i,0],sampleset[i,1],densityfunc(sampleset[i])])
+        samppoints.append(samplePoint([0,0],[[1,0],[0,1]],sampleset[i,0],sampleset[i,1]))
+    fig=plt.figure()
+    ax=fig.gca(projection='3d')
+    ax.scatter([x[0] for x in samppoints],[x[1] for x in samppoints],[x[2] for x in samppoints])
+    ax.scatter([x[0] for x in estipoints],[x[1] for x in estipoints],[x[2] for x in estipoints])
+    plt.savefig('./knearest.png')
+    plt.show()
--- ParzenWindow/NonParameterEstimate.py
+++ ParzenWindow/NonParameterEstimate.py
@@ -12,9 +12,10 @@ def cubeWinFunc(vector):
     return result
 def expWinFunc(vector):
     return math.exp(-np.linalg.norm(vector))
-def parzenEstimate(sampleSet,width,winFunc=normalWinFunc):
-    n,d=sampleSet.shape()
-    v=(width/n**0.5)**d
+def parzenEstimate(sampleSet,width,winFunc=cubeWinFunc):
+    n,d=sampleSet.shape
+    width=width/n**0.5
+    v=width**d
     def probability(sample):
         s=0
         for i in range(0,n):
@@ -22,4 +23,29 @@ def parzenEstimate(sampleSet,width,winFunc=normalWinFunc):
         return s/n
     return probability
 if __name__=='__main__':
-    pass
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib.ticker import LinearLocator, FormatStrFormatter
+    def samplePoint(mean,cov,x0,x1):
+        x=np.mat([x0,x1])
+        mean=np.mat(mean)
+        cov=np.mat(cov)
+        p=len(mean)
+        y=(2*math.pi)**(p/(-2))
+        y=y*(np.linalg.det(cov))**(-0.5)
+        t=(x-mean)*np.linalg.inv(cov)*(x-mean).T
+        y=y*np.e**(-0.5*t[0,0])
+        return [x0,x1,y]
+    sampleset=np.random.multivariate_normal(mean=[0,0],cov=[[1,0],[0,1]],size=1024)
+    densityfunc=parzenEstimate(sampleset,4)
+    estipoints=[]
+    samppoints=[]
+    for i in range(0,64):
+        estipoints.append([sampleset[i,0],sampleset[i,1],densityfunc(sampleset[i])])
+        samppoints.append(samplePoint([0,0],[[1,0],[0,1]],sampleset[i,0],sampleset[i,1]))
+    fig=plt.figure()
+    ax=fig.gca(projection='3d')
+    ax.scatter([x[0] for x in samppoints],[x[1] for x in samppoints],[x[2] for x in samppoints])
+    ax.scatter([x[0] for x in estipoints],[x[1] for x in estipoints],[x[2] for x in estipoints])
+    plt.savefig('./parzen.png')
+    plt.show()
