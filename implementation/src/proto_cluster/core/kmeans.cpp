31772273832ad3da86ca424ba7e2bb076c584726
Patrick <dawnerz@163.com>
2019-10-27 17:41:05 +0800

    kmeans cluster

--- implementation/src/proto_cluster/core/kmeans.cpp
+++ implementation/src/proto_cluster/core/kmeans.cpp
@@ -24,5 +24,51 @@ void KMeans::cluster(double *data, unsigned long width, unsigned long length)
     {
         centers.push_back(datas[*iter]);
     }
-    
+    //iterate
+    unsigned long update=k;
+    unsigned long repeat=0;
+    while (update!=0 && repeat<maxRepeat)
+    {
+        repeat++;
+        update=0;
+        std::vector<Vector>::iterator iter=datas.begin();
+        //update clusters
+        for (unsigned long i = 0; i < k; i++)
+        {
+            //clear
+            clusters[i].clear();
+        }
+        for (; iter != datas.end(); iter++)
+        {
+            unsigned long cluster=0;
+            double distance=distanceFunc(*iter,centers[0]);
+            for (unsigned long i = 1; i < centers.size(); i++)
+            {
+                double newDist=distanceFunc(*iter, centers[i]);
+                if (newDist<distance)
+                {
+                    distance=newDist;
+                    cluster=i;
+                }
+            }
+            //insert
+            clusters[cluster].push_back(*iter);
+        }
+        //update centers
+        for (unsigned long i = 0; i < k; i++)
+        {
+            Vector newCenter{width};
+            std::vector<Vector>::iterator iter=clusters[i].begin();
+            for ( ; iter != clusters[i].end(); iter++)
+            {
+                newCenter+=*iter;
+            }
+            newCenter=newCenter*(1/clusters[i].size());
+            if (distanceFunc(newCenter, centers[i])>minDist)
+            {
+                update++;
+                centers[i]=newCenter;
+            }
+        }
+    }
 }
--- implementation/src/proto_cluster/core/kmeans.h
+++ implementation/src/proto_cluster/core/kmeans.h
@@ -7,9 +7,12 @@ namespace patrick
     class KMeans : public Cluster
     {
     private:
-        DistanceFunc& distance;
+        DistanceFunc& distanceFunc;
+        unsigned long maxRepeat;
+        double minDist;
     public:
-        KMeans(unsigned long k, DistanceFunc& dist) : Cluster{k}, distance{dist}{};
+        KMeans(unsigned long k, DistanceFunc& dist, unsigned long maxRepeat=100, double minDist=0.1) 
+            : Cluster{k}, distanceFunc{dist}, maxRepeat{maxRepeat}, minDist{minDist} {};
         void cluster(double *data, unsigned long width, unsigned long length);
     };
     
