2e4b78d9c5dffac3f14e63ec03ea65584c2bd2cb
Patrick <dawnerz@163.com>
2019-11-03 19:50:39 +0800

    deep feedforward

---
+++ implementation/src/svm/core/svm.h
@@ -0,0 +1,12 @@
+#ifndef CORE_SVM_H
+#define CORE_SVM_H
+#include "./../../common/core/classifier.h"
+namespace patrick
+{
+    class SVM : Classifier
+    {
+
+    }
+} // namespace patrick
+
+#endif
